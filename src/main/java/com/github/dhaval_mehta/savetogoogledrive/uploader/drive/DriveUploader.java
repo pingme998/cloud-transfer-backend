81720de8e49243a3cdf6a6c4fd7d331cd5f7d5d6
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-05-23 20:03:51 +0530

    Update DriveUploader.java

--- src/main/java/com/github/dhaval_mehta/savetogoogledrive/uploader/drive/DriveUploader.java
+++ src/main/java/com/github/dhaval_mehta/savetogoogledrive/uploader/drive/DriveUploader.java
@@ -56,7 +56,7 @@ abstract class DriveUploader implements Uploader {
 	}
 
 	private DriveUploader() {
-		chunkSize = 10 * 1024 * 1024;
+		chunkSize = 30 * 1024 * 1024;
 		uploadInformation = new UploadInformation();
 		uploadInformation.setUploadStatus(UploadStatus.waiting);
 
